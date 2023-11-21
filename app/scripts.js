36e7c61020f5fcf92338ee75a8118572e4819296
Justin Golden <justingolden21@gmail.com>
2023-11-20 21:49:42 -0800

    Use new clipboard api
    TODO: doesn't work since nothing is focused

--- app/scripts.js
+++ app/scripts.js
@@ -1,9 +1,19 @@
 window.onload = function () {
 	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+		function successCopy() {
+			document.getElementById('info').innerHTML =
+				'Link copied successfully';
+		}
+
 		const urlArea = document.getElementById('urlArea');
 		urlArea.innerHTML = tabs[0].url;
 		urlArea.select();
-		document.execCommand('copy');
-		document.getElementById('info').innerHTML = 'Link copied successfully';
+
+		if (navigator.clipboard) {
+			navigator.clipboard.writeText(tabs[0].url).then(successCopy);
+		} else {
+			document.execCommand('copy');
+			successCopy();
+		}
 	});
 };
